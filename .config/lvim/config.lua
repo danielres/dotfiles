bc654cd1a95a7e781343ef9fa18dcddc6872168e
Daniel Reszka <dannn.r@gmail.com>
2024-03-08 14:18:38 +0100

    add .zshrc, .bashrc, lvim, xremap

---
+++ .config/lvim/config.lua
@@ -0,0 +1,53 @@
+-- Read the docs: https://www.lunarvim.org/docs/configuration
+-- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
+-- Forum: https://www.reddit.com/r/lunarvim/
+-- Discord: https://discord.com/invite/Xb9B4Ny
+--
+
+
+lvim.format_on_save.enabled = true
+
+
+-- Add Copilot plugin
+-- https://medium.com/aimonks/a-guide-to-integrating-lunarvim-github-copilot-61d92f764913
+lvim.plugins = {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({})
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    after = { "copilot.lua" },
+    config = function()
+      require("copilot_cmp").setup()
+    end,
+  },
+}
+-- End add Copilot plugin
+
+
+-- Configure Copilot
+local ok, copilot = pcall(require, "copilot")
+if not ok then
+  return
+end
+
+copilot.setup {
+  suggestion = {
+    keymap = {
+      accept = "<c-l>",
+      next = "<c-j>",
+      prev = "<c-k>",
+      dismiss = "<c-h>",
+    },
+  },
+  -- copilot_node_command = "/nix/store/vz13mi0w75q96sfjxz2ylnv8812hvf34-nodejs-20.11.1/bin/node",
+}
+
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<c-s>", "<cmd>lua require('copilot.suggestion').toggle_auto_trigger()<CR>", opts)
+-- End configure Copilot
---
+++ .config/xremap/service.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/daniel/.local/bin/xremap /home/daniel/.config/xremap/config.yml --watch=config #--device /dev/input/event7
