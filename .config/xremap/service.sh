5f8034ba2da36871f47dab2c45621f5df6bea72a
Daniel Reszka <dannn.r@gmail.com>
2024-03-20 13:54:56 +0100

    xremap service: disable

--- .config/xremap/service.sh
+++ .config/xremap/service.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/daniel/.local/bin/xremap /home/daniel/.config/xremap/config.yml --watch=config #--device /dev/input/event7
+# /home/daniel/.local/bin/xremap /home/daniel/.config/xremap/config.yml --watch=config #--device /dev/input/event7
