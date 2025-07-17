2e6408943e6707c2fe6439fef6280422f99ffc99
Daniel Reszka <dannn.r@gmail.com>
2025-07-17 21:22:23 +0200

    fish: disable atuin

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,5 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     fish_vi_cursor
     fish_vi_key_bindings
-    atuin init fish | source
+    # atuin init fish | source
 end
