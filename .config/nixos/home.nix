589d97fc00527d7fa9ad044fce48d4986a940d18
= <=>
2025-07-18 13:07:05 +0200

    home.nix: set vim env vars

--- .config/nixos/home.nix
+++ .config/nixos/home.nix
@@ -10,6 +10,12 @@
 
   home.username = "daniel";
   home.homeDirectory = "/home/daniel";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    NVIM_APPNAME="vim-lazyvim";
+  };
+
   home.packages = with pkgs; [
     # nixvim.packages.${pkgs.system}.default
     # nix-software-center.packages.${pkgs.system}.default
