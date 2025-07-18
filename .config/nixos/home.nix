74c547502a22927d6327986782ddea28be9273ed
= <=>
2025-07-18 13:06:52 +0200

    home.nix: set username + home dir

--- .config/nixos/home.nix
+++ .config/nixos/home.nix
@@ -8,6 +8,8 @@
 {
   home.stateVersion = "25.05";
 
+  home.username = "daniel";
+  home.homeDirectory = "/home/daniel";
   home.packages = with pkgs; [
     # nixvim.packages.${pkgs.system}.default
     # nix-software-center.packages.${pkgs.system}.default
