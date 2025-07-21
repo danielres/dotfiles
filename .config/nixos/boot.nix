405498c3482d41b4ddfed5e99d1f64415b55af58
Daniel Reszka <dannn.r@gmail.com>
2025-07-21 20:58:13 +0200

    extract boot.nix

---
+++ .config/nixos/boot.nix
@@ -0,0 +1,15 @@
+{ }: {
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+
+  # boot.kernelModules = [ "amdgpu" ];
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # boot.kernelParams = [ "video=1280x720" ];
+
+  boot.initrd.luks.devices."nixoscrypt".device = "/dev/nvme0n1p5";
+  boot.resumeDevice = "/dev/disk/by-uuid/cba6b9b8-55cf-46eb-9122-84dd99cb0138";
+  swapDevices = [{ device = "/dev/nixos/swap"; }];
+  boot.kernelParams = [ "resume=UUID=cba6b9b8-55cf-46eb-9122-84dd99cb0138" ];
+}
--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -8,6 +8,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./boot.nix
     ./locales.nix
     ./hibernate.nix
     ./root.nix
@@ -17,16 +18,6 @@
 
   # nix.settings.auto-optimize-store = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  # boot.kernelModules = [ "amdgpu" ];
-  boot.loader.efi.canTouchEfiVariables = true;
-  #boot.kernelParams = [ "video=1280x720" ];
-  boot.initrd.luks.devices."nixoscrypt".device = "/dev/nvme0n1p5";
-  boot.resumeDevice = "/dev/disk/by-uuid/cba6b9b8-55cf-46eb-9122-84dd99cb0138";
-  swapDevices = [{ device = "/dev/nixos/swap"; }];
-  boot.kernelParams = [ "resume=UUID=cba6b9b8-55cf-46eb-9122-84dd99cb0138" ];
 
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
