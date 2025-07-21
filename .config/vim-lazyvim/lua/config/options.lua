140cf8993930edd5418e055a879fc6a1182e42a6
Daniel Reszka <dannn.r@gmail.com>
2025-07-21 20:46:41 +0200

    lazyvim: disable cursor blinking

--- .config/vim-lazyvim/lua/config/options.lua
+++ .config/vim-lazyvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 -- Add any additional options here
 
 vim.o.shell = "fish"
+
+-- vim.o.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkon0"
+-- Disable lazyvim's default guicursor and set custom one
+-- vim.o.guicursor = ""  -- clear defaults
+-- vim.opt.guicursor = {
+--   "n-v-c-sm:block-Cursor/lCursor",
+--   "i-ci-ve:ver25-Cursor/lCursor",
+--   "r-cr-o:hor20-Cursor/lCursor",
+--   "a:blinkon0"
+-- }
