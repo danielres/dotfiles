4c748fbf215a5d9da6beb1f87b835af82b1f84fe
Daniel Reszka <dannn.r@gmail.com>
2025-11-11 14:15:35 +0100

    flake: set hyprland input/output

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -7,9 +7,10 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    hyprland.url = "github:hyprwm/Hyprland";
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, hyprland, ... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
         specialArgs = { inherit inputs; };
