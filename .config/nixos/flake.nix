18712b96af434debd2df37eaf8b28cc6f9ef20f5
Daniel Reszka <dannn.r@gmail.com>
2026-03-15 20:01:52 +0100

    flake.nix: comment leftover hyprland config

--- .config/nixos/flake.nix
+++ .config/nixos/flake.nix
@@ -7,46 +7,53 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    hyprland.url = "github:hyprwm/Hyprland";
-    hyprland-plugins = {
-      url = "github:hyprwm/hyprland-plugins";
-      inputs.hyprland.follows = "hyprland";
-    };
+    # hyprland.url = "github:hyprwm/Hyprland";
+    # hyprland-plugins = {
+    #   url = "github:hyprwm/hyprland-plugins";
+    #   inputs.hyprland.follows = "hyprland";
+    # };
   };
 
-  outputs = { nixpkgs, home-manager, hyprland, ... }@inputs: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        system = "x86_64-linux";
-        modules = [
-          ./hardware-configuration.nix
-          ./boot.nix
-          ./locales.nix
-          ./hibernate.nix
-          ./root.nix
-          # ./gnome.nix
-          ./system-packages.nix
-          ./configuration.nix
-          # ./hyprland-greetd.nix
-          ./niri-greetd.nix
-          ./kde-connect.nix
-        ];
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      # hyprland,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          system = "x86_64-linux";
+          modules = [
+            ./hardware-configuration.nix
+            ./boot.nix
+            ./locales.nix
+            ./hibernate.nix
+            ./root.nix
+            # ./gnome.nix
+            ./system-packages.nix
+            ./configuration.nix
+            # ./hyprland-greetd.nix
+            ./niri-greetd.nix
+            ./kde-connect.nix
+          ];
+        };
       };
-    };
 
-    homeConfigurations = {
-      daniel = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-        # pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        extraSpecialArgs = { inherit inputs; };
-        modules = [
-          ##
-          ../home-manager/home.nix
-          # ../home-manager/home-hyprland.nix
-          ../home-manager/home-niri.nix
-        ];
+      homeConfigurations = {
+        daniel = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+          # pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          extraSpecialArgs = { inherit inputs; };
+          modules = [
+            ##
+            ../home-manager/home.nix
+            # ../home-manager/home-hyprland.nix
+            ../home-manager/home-niri.nix
+          ];
+        };
       };
     };
-  };
 }
