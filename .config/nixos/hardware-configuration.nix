188aca7981b86f41550e28a68066c524d467884b
Daniel Reszka <dannn.r@gmail.com>
2026-03-15 16:34:52 +0100

    hardware-configuration.nix: add xone controller config (commented)

--- .config/nixos/hardware-configuration.nix
+++ .config/nixos/hardware-configuration.nix
@@ -52,4 +52,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # Enable the Xone driver for Xbox One controllers.
+  # hardware.xone.enable = true;
 }
